0c47d1ab13bc35a4ab0aa87d12c3decd9ff0a158
nookhi <62215265+nookhi@users.noreply.github.com>
2020-10-13 00:01:11 +0300

    Add files via upload

---
+++ SecondPractice/SecondPractice/SecondPractice.py
@@ -0,0 +1,33 @@
+# 3й вариант
+# 2е задание
+import random
+import numpy as np
+def sortingStrings(a):
+    return sorted(list(a))
+b=sortingStrings('This string needs to be sorted')
+print("".join(b))
+
+# 4е задание
+def randomArrays(arrayQuantity, numbersQuantity):
+    m=np.random.randint(0, 10, (arrayQuantity, numbersQuantity))
+    print(m)
+    max_idx, max_sum = 0, 0
+    i=0
+    while i<arrayQuantity:
+        if sum(m[i]) > max_sum:
+            max_sum = sum(m[i])
+            max_idx = i
+        i=i+1
+    return m[max_idx]
+x = randomArrays(3, 3)
+print(x)
+# 10е задание
+def secondRandomArrays(arraySize):
+    arr = np.random.sample(arraySize)
+    values = (min(arr), max(arr))
+    return values
+values = secondRandomArrays(50)
+maxValue = values[0]
+minValue = values[1]
+print(maxValue, minValue)
+
