978e4a4a24c70ffa258cb0c7e6e5d4024c6fae0e
nookhi <62215265+nookhi@users.noreply.github.com>
2020-10-16 17:29:24 +0300

    Update SecondPractice.py

--- SecondPractice/SecondPractice/SecondPractice.py
+++ SecondPractice/SecondPractice/SecondPractice.py
@@ -10,24 +10,15 @@ print("".join(b))
 # 4е задание
 def randomArrays(arrayQuantity, numbersQuantity):
     array=np.random.randint(0, 10, (arrayQuantity, numbersQuantity))
-    print(array)
-    max_idx, max_sum = 0, 0
-    i=0
-    while i<arrayQuantity:
-        if sum(array[i]) > max_sum:
-            max_sum = sum(array[i])
-            max_idx = i
-        i=i+1
-    return array[max_idx]
+    arraySum = np.sum(array, axis = 1)
+    maxArg = np.argmax(arraySum)
+    return array[maxArg]
 x = randomArrays(3, 3)
 print(x)
 # 10е задание
 def secondRandomArrays(arraySize):
     arr = np.random.sample(arraySize)
-    values = (min(arr), max(arr))
-    return values
-values = secondRandomArrays(50)
-maxValue = values[0]
-minValue = values[1]
+    return return min(arr), max(arr)
+maxValue, minValue = secondRandomArrays(50)
 print(maxValue, minValue)
 
