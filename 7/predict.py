b56c35214bbc5cd92d8d94b30674138ebf015f75
LacteaVia322 <62036565+LacteaVia322@users.noreply.github.com>
2020-11-30 17:58:03 +0300

    Update predict.py

--- 7/predict.py
+++ 7/predict.py
@@ -2,13 +2,9 @@ import numpy as np
 from sigmoid import sigmoid
 
 def predict (X, Theta1, Theta2):
-    m=X.shape[0]
-    ones=np.ones(m), X
-    a1=np.c_[ones]
-    z2=np.dot(a1,Theta1.T)
-    a2=sigmoid(z2)
-    z3=np.ones(a2.shape[0])
-    a3=np.c_[z3, a2]
+    a1=np.c_[np.ones(X.shape[0]), X]
+    a2=sigmoid(np.dot(a1,Theta1.T))
+    a3=np.c_[np.ones(a2.shape[0]), a2]
     h_x = sigmoid(np.dot(a3,Theta2.T))
     p = np.argmax(h_x, axis=1)+1
     return  p
